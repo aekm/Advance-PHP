3b7e3000f72dd34135639bd2bf1bda269707689a
aekm <56739195+aekm@users.noreply.github.com>
2020-11-20 05:16:47 -0800

    Create model_index.php

---
+++ models/model_index.php
@@ -0,0 +1,13 @@
+<?php
+
+class model_index extends Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    #you can define new function and connect it to relate controller (controller/index.php)
+
+	
+}
