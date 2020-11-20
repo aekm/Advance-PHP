85f0990d9d97623f6c80f70c21d7760fbaacbe10
aekm <56739195+aekm@users.noreply.github.com>
2020-11-20 05:08:07 -0800

    Create index.php

---
+++ controllers/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class Index  extends Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index(){
+
+        $this->view('index/index',@$data,'','');
+
+    }
+    
+
+}
+
+
+
+
