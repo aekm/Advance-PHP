35fd50fcc1624227c7777a4ba38617fffab30403
aekm <56739195+aekm@users.noreply.github.com>
2020-11-20 05:04:08 -0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <base href="<?= URL ?>">
+    <meta charset="utf-8">
+
+    <title>MVC-TEST</title>
+
+
+    <style>
+ 	
+	/* your header styles here */
+
+    </style>
+
+</head>
+
+<body>
+
+
+	<header>
+
+	</header>
+
+	
+	
