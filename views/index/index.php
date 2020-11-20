fbf8b70b91b71d47f05fd0286ac485269ae5618f
aekm <56739195+aekm@users.noreply.github.com>
2020-11-20 05:18:40 -0800

    Create index.php

---
+++ views/index/index.php
@@ -0,0 +1,9 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="<?= URL ?>public/css/your-css-file.css">
+<script src="<?= URL ?>public/js/your-js-file.js"></script>
+
+
+
+
+
+<!-- you can insert your html code here without any <html> or <body> tag before that -->
